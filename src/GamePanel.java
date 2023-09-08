d5acaef1f7ea3faad8c959ddcba9c234d2911399
stefan <stefanmihairahlitchi@gmail.com>
2023-09-08 18:59:39 +0300

    Commit3

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,11 +1,33 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
+import java.sql.*;
+import java.util.Random;
 public class GamePanel extends JPanel implements ActionListener {
 
-    GamePanel() {
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH* SCREEN_HEIGHT) /UNIT_SIZE;
+    static final int DELAY = 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appley;
+    char direction = 'R';
+    boolean running = false;
+    Time time;
+    Random random;
 
+    GamePanel() {
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
 
     public void startGame() {
@@ -17,6 +39,10 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void draw(Graphics g) {
+
+    }
+
+    public void newApple() {
 
     }
 
@@ -38,5 +64,13 @@ public class GamePanel extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
          // TODO Auto-generated method sub
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
     }
 }
