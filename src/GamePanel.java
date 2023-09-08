f778729b516c6ff40e7d338b7da463f181560f16
stefan <stefanmihairahlitchi@gmail.com>
2023-09-08 16:54:49 +0300

    Commit1

---
+++ src/GamePanel.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+         //TODO
+    }
+}
