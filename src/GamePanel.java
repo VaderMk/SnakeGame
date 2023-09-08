5a3caea57097b6292061e0ba8aba89088f533f46
stefan <stefanmihairahlitchi@gmail.com>
2023-09-08 17:55:28 +0300

    Commit2

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,10 +1,42 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 public class GamePanel extends JPanel implements ActionListener {
 
+    GamePanel() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollision() {
+
+    }
+
+    public void gameOver(Graphics g) {
+
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
-         //TODO
+         // TODO Auto-generated method sub
     }
 }
